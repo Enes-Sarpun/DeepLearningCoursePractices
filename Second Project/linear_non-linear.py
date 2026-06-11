1f0efc2c881812ed5d50c4996334f3753a7b50f5
Enes Sarpün <enessarpun63@gmail.com>
2026-06-11 16:09:07 +0300

    New files uploaded

---
+++ Second Project/linear_non-linear.py
@@ -0,0 +1,17 @@
+# Import necessary libraries;
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load the dataset;
+data = pd.read_csv("email_classification_svm.csv")
+data2 = pd.read_csv("seismic_activity_svm.csv")
+
+
+
+
+
+
